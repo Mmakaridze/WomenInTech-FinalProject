01971a2b35184b5c404f731c2bbab702910807ac
Mmakaridze <93215227+Mmakaridze@users.noreply.github.com>
2023-01-08 19:13:06 +0400

    დავამატე სერვერიდან ინფოს წამოღება

--- js/script.js
+++ js/script.js
@@ -134,3 +134,58 @@ contactUs.addEventListener ("submit", function (event) {
         contactUs.submit();
       }
 })
+
+// fetch
+
+let currentPage = 1;
+let post = document.getElementById("vet-post");
+
+function getUsers(page){
+    fetch("https://reqres.in/api/users?page=" + page, {
+    METHOD: "GET"
+})
+
+.then (function(text) {
+    if (text.status !== 200) {
+        throw text.status;
+    }
+    return text.json();
+})
+
+.then (function (convertedJs) {
+    convertedJs.data.forEach((item) => {
+        let user = document.createElement("p");
+        user.classList.add("post1-p");
+        user.innerText = `${item.first_name} ${item.last_name}`;
+
+        let avatar = document.createElement("img");
+        avatar.src = item.avatar;
+        avatar.setAttribute("alt", "avatar");
+        avatar.classList.add("avatar");
+
+        let nameAvatarWraper = document.createElement("div");
+        nameAvatarWraper.classList.add("postvet-div");
+        let nameAvatar = document.createElement("div");
+
+        nameAvatar.appendChild(user);
+        nameAvatar.appendChild(avatar);
+        nameAvatarWraper.appendChild(nameAvatar);
+        post.appendChild(nameAvatarWraper);
+    });
+})
+.catch(function(error){
+    if(error == 404){
+        let p = document.createElement("p");
+        p.textContent = "page not found";
+        p.classList.add("text");
+        post.appendChild(p);
+    }
+});
+}
+let loadMore = document.getElementById("loadmore");
+loadMore.addEventListener("click", function(){
+    currentPage++;
+    getUsers(currentPage);
+});
+
+getUsers(currentPage);
